3ac512b5c01d4264ce25f4e37df56122ceac2175
alexei <dorofeev86@yahoo.com>
2020-11-29 16:08:24 +0100

    installed typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { truncate } from 'fs/promises';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
@@ -9,6 +10,16 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       autoSchemaFile: true,
     }),
     RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123',
+      database: 'uber-eats',
+      synchronize: true,
+      logging: true,
+    }),
   ],
   controllers: [],
   providers: [],
