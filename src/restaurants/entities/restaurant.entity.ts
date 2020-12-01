4be21c55ac7be5024bc126a52fa3876452dec3d8
alexei <dorofeev86@yahoo.com>
2020-12-01 19:37:07 +0100

    added fields and columns

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,16 +1,30 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Restaurant {
+  @Field(() => Number)
+  @PrimaryGeneratedColumn()
+  id: number;
+
   @Field(() => String)
+  @Column()
   name: string;
 
   @Field(() => Boolean)
+  @Column()
   isVegan: boolean;
 
   @Field(() => String)
+  @Column()
   address: string;
 
   @Field(() => String)
+  @Column()
   ownersName: string;
+
+  @Field(() => String)
+  @Column()
+  categoryName: string;
 }
