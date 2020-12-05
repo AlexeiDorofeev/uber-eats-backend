c8d6dc0e1ba181bb6829ce19f7fe1a3aa74f9849
alexei <dorofeev86@yahoo.com>
2020-12-05 17:50:39 +0100

    handle errors

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,8 +1,14 @@
-import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { Field, InputType, ObjectType, registerEnumType } from '@nestjs/graphql';
 import { CoreEntity } from 'src/common/entities/core.entity';
 import { Column, Entity } from 'typeorm';
 
-type UserRole = 'client' | 'owner' | 'delivery';
+enum UserRole {
+  Client,
+  Owner,
+  Delivery,
+}
+
+registerEnumType(UserRole, { name: 'UserRole' });
 
 @InputType({ isAbstract: true })
 @ObjectType()
@@ -12,11 +18,11 @@ export class User extends CoreEntity {
   @Field(() => String)
   email: string;
 
-  @Field(() => String)
   @Column()
+  @Field(() => String)
   password: string;
 
-  @Field(() => String)
-  @Column()
+  @Column({ type: 'enum', enum: UserRole })
+  @Field(() => UserRole)
   role: UserRole;
 }
