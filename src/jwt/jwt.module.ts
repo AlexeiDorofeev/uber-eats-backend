1ec25ba51d3c9f8acb5fdf9083f740759f2a4663
alexei <dorofeev86@yahoo.com>
2020-12-06 12:32:09 +0100

    created own jwt module

--- src/jwt/jwt.module.ts
+++ src/jwt/jwt.module.ts
@@ -1,14 +1,21 @@
 import { DynamicModule, Global, Module } from '@nestjs/common';
+import { JwtModuleOptions } from './jwt.interfaces';
 import { JwtService } from './jwt.service';
 
 @Module({})
 @Global()
 export class JwtModule {
-  static forRoot(): DynamicModule {
+  static forRoot(options: JwtModuleOptions): DynamicModule {
     return {
       module: JwtModule,
+      providers: [
+        {
+          provide: 'BANANAS',
+          useValue: options,
+        },
+        JwtService,
+      ],
       exports: [JwtService],
-      providers: [JwtService],
     };
   }
 }
